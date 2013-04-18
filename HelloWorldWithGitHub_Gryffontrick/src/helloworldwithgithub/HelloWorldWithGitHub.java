83f5fb884ba69f9e109100d234fd886ca2ac73a0
NielsFruhauf <Miysis@Miysis-PC>
2013-04-18 21:53:30 +0200

    test

---
+++ HelloWorldWithGitHub_Gryffontrick/src/helloworldwithgithub/HelloWorldWithGitHub.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworldwithgithub;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Jonathan et Niels depuis le compte de Niels
+ */
+public class HelloWorldWithGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            System.out.print("Quel est votre nom? ");
+            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+            String var = input.readLine();
+            System.out.println("Bonjour " + var + "!");
+        } catch (IOException ex) {
+            Logger.getLogger(HelloWorldWithGitHub.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
